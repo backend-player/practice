d348a76174ab2c757c9dbdfaad1b815c50720bb8
backend-player <rizkylev@gmail.com>
2022-07-17 00:23:46 +0700

    navbar

---
+++ tiket_minibus/navbar.php
@@ -0,0 +1,8 @@
+<div class="navbar">
+    <ul>
+        <li><a href="index.php">Pesan tiket</a></li>
+        <li><a href="bayar-tiket.php">Bayar tiket</a></li>
+        <li style="float:right"><a href="data-bus-dan-penumpang.php">Data penumpang</a></li>
+
+    </ul>
+</div>
